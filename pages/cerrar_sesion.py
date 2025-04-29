bd739848ab3ffbad4c219c8124745baebff720cf
OrtsInvesting <ortsinvesting@gmail.com>
2025-04-29 16:31:55 +0200

    Update cerrar_sesion.py

--- pages/cerrar_sesion.py
+++ pages/cerrar_sesion.py
@@ -1,1 +1,13 @@
+import streamlit as st
+
+def mostrar():
+    st.title("🔚 Cerrar sesión")
+
+    st.success("👋 Has cerrado sesión correctamente.")
+    
+    # Limpiar todo el session_state
+    st.session_state.clear()
+
+    # Rerun para volver a login
+    st.experimental_rerun()
 
